cd4ba3bd253d09f2d2b0c38ef3b16f60c68840d3
DalitsoSakala <dalitso.1sc@gmail.com>
2021-11-08 19:19:52 +0200

    Added detailed licencing, now *v 0.0.4*

--- test/flutter_basics_test.dart
+++ test/flutter_basics_test.dart
@@ -1,9 +1,16 @@
+/// Copyright (c) 2021 Jedi Hero https://www.github.com/jedi-hero.
+/// All rights reserved.
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_basics/flutter_basics.dart';
 
+/**
+ * Copyright (c) 2021 Jedi Hero https://www.github.com/jedi-hero.
+ * All rights reserved.
+ */
 void main() {
- /* test('adds one to input values', () {
+  /* test('adds one to input values', () {
     final calculator = Calculator();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
