1409fe1db5b09bae2fba5a27548cce143390a476
DalitsoSakala <dalitso.1sc@gmail.com>
2022-01-08 13:57:33 +0200

    releasing **v1.3.4+17**, now with extensions

---
+++ lib/extensions.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Jedi Hero
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import 'package:flutter/material.dart';
+
+extension on GlobalKey {
+  Size? get size_ {
+    final box = currentContext?.findRenderObject() as RenderBox?;
+    return box?.size;
+  }
+}
--- lib/flutter_basics.dart
+++ lib/flutter_basics.dart
@@ -1,4 +1,3 @@
-
 /// Commonly used custom flutter mixins, classes and functions in the development cycle.
 /// The library provides minimal population of the scope to avoid cluttering and unnecessary ambiguity.
 /// It attemts to solve otherwise routine configurations which may be too involving.
@@ -6,3 +5,5 @@ library flutter_basics;
 
 export 'mixins.dart';
 export 'models.dart';
+export 'utilities.dart';
+export 'extensions.dart';
