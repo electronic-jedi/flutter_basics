68c253a0496dc0f0bd7d832a5628a04a1a3d0934
Dalitso Sakala <dalitso.1sc@gmail.com>
2023-02-19 21:51:33 +0200

    Introduction

--- lib/mixins.dart
+++ lib/mixins.dart
@@ -5,94 +5,73 @@
 
 library mixins;
 
-import 'package:flutter/material.dart'
-    show
-        BuildContext,
-        MediaQuery,
-        MediaQueryData,
-        ScaffoldMessenger,
-        ScaffoldMessengerState,
-        TextTheme,
-        Theme,
-        ThemeData,
-        Widget;
+import 'package:flutter/material.dart';
 
-/// Utility to ease navigation even on a single widget.
-@deprecated
-mixin NavigatorSetup {
-  /// Handle navigation when we want to satisfy the conditions of moving from a view.
-  void setViewIndex(int index,
-      {required int target,
-      String? customTitle,
-      List<Widget> actions = const [],
-      Map<dynamic, dynamic>? extras}) {
-    navigatorStateSetup(() {
-      viewTarget_ = target;
-      viewIndex_ = index;
-      customTitle_ = customTitle;
-      actions_ = actions;
-      extras_ = extras ?? {};
-    });
-  }
-
-  /// Typically the method [StatefulWidget.setState].
-  /// This enables the [NavigatorSetup] fields to be updated with the state of the [StatefulWidget]
-  void Function(Function()) get navigatorStateSetup;
-
-  /// Provides mobility for the commonly used [WillPopScope] widget.
-  /// This can  be optionally assigned to the [WillPopScope.onWillPop] argument of the widget.
-  /// It only serves as an option and you can choose not to use it.
-  Future<bool> onWillPop_() {
-    var pop = false;
-    if (viewTarget_ > 0) {
-      if (viewIndex_ > 0) {
-        setViewIndex(viewIndex_ - 1, target: viewTarget_);
-      } else {
-        setViewIndex(0, target: 0);
-      }
-    } else if (viewIndex_ > 0) {
-      setViewIndex(viewIndex_ - 1, target: 0);
-    } else {
-      pop = true;
-    }
-    return Future.value(pop);
-  }
-
-  /// Indicates target view section that can have different levels.
-  /// The various levels are assigned values of [viewIndex_].
-  /// The [viewTarget_] can have the same value over different values of
-  /// [viewIndex_]
-  int viewTarget_ = 0;
-
-  /// Indicates target various navigation depths in a [viewTarget_] aka [target_].
-  /// When the [viewIndex_]==0, another call to [Navigator.pop] has he implication
-  /// that the [viewTarget_] aka [target_] has to be changed
-  var viewIndex_ = 0;
-
-  String? customTitle_;
-
-  List<Widget> actions_ = [];
-
-  Map<dynamic, dynamic> extras_ = {};
+class EJOverlaySettings {
+  const EJOverlaySettings({
+    this.top,
+    this.right,
+    this.bottom,
+    this.left,
+    this.width,
+    this.height,
+    this.alignment = AlignmentDirectional.topStart,
+    this.emptyAreaTap,
+  });
+  final double? top;
+  final double? right;
+  final double? bottom;
+  final double? left;
+  final double? width;
+  final double? height;
+  final AlignmentGeometry alignment;
+  final void Function()? emptyAreaTap;
 }
 
-/// Used to enable the visibility commonly used ui objects.
-/// One must include a call to [WidgetBasics.onDidChangeDependencies_] before using the
-/// exposed objects namely [WidgetBasics.typography_], [WidgetBasics.theme_] and [WidgetBasics.media_]
-@deprecated
-mixin WidgetBasics {
-  /// This method must be called.
-  /// Call this method in the [StetefulWidget.didChangeDependencies] stateful widget
-  /// method callback to initialize  [typography_], [theme_] and [media_]
-  void onDidChangeDependencies_(BuildContext context) {
-    theme_ = Theme.of(context);
-    typography_ = theme_.textTheme;
-    media_ = MediaQuery.of(context);
-    scaffold_ = ScaffoldMessenger.of(context);
+mixin EJOverlay {
+  OverlayEntry? _entry;
+  BuildContext get context;
+  Widget get ejOverlayContent;
+  void Function(void Function() fn) get ejSetState;
+  EJOverlaySettings get ejOverlaySettings;
+
+  void ejShowOverlay() {
+    _buildOverlay();
+    Overlay.of(context).insert(_entry!);
   }
 
-  late TextTheme typography_;
-  late ThemeData theme_;
-  late MediaQueryData media_;
-  late ScaffoldMessengerState scaffold_;
+  void ejRemoveOverlay() {
+    if (_entry?.mounted ?? false) _entry!.remove();
+  }
+
+  void _buildOverlay() {
+    var d = ejOverlaySettings;
+    OverlayEntry e;
+    void Function() eat = d.emptyAreaTap ?? ejRemoveOverlay;
+
+    e = OverlayEntry(
+      builder: (_) => Material(
+        color: Colors.transparent,
+        child: GestureDetector(
+          onTap: eat,
+          child: Stack(
+            children: [
+              Positioned(
+                left: d.left,
+                top: d.top,
+                right: d.right,
+                bottom: d.bottom,
+                child: SizedBox(
+                  width: d.width,
+                  height: d.height,
+                  child: ejOverlayContent,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+    ejSetState(() => _entry = e);
+  }
 }
